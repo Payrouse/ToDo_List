562f14b5532fcb52b041277b283a400cd8685a2f
Payrouse <leonardobone99@gmail.com>
2020-12-25 12:54:39 -0500

    Mi primer commit

---
+++ app/src/main/java/com/c16/todo_list/MainActivity.java
@@ -0,0 +1,14 @@
+package com.c16.todo_list;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
